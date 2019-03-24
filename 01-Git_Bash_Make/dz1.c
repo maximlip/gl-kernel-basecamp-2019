07c63bd748568738467e5c83f84de82f5d63ccfb
Maxim Lipchanskyi <m4554747@gmail.com>
2019-03-25 01:19:06 +0200

    Initial commit
    add new file dz1.c
    
    Signed-off-by: Maxim Lipchanskyi <m4554747@gmail.com>

--- 01-Git_Bash_Make/dz1.c
+++ 01-Git_Bash_Make/dz1.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-
-  return 0;
+    int d, r;
+    
+    srand(time(NULL));
+    printf("Enter number 0-9>");
+    scanf("%d", &d);
+    r = rand()%10;
+   // printf("%d %d\n", d, r);
+    if (d == r) {
+        printf("You win\n");
+    }
+    else {
+        printf("You loose\n");
+    }
+    
+    return 0;
   
 }
 
